b8c4c99ac1781ecbb88cbaca68cb626fb4f07754
Ariel-Alejandr0 <pessoal.ariel.alejandro@gmail.com>
2023-10-08 15:35:10 -0300

    versão funcional

--- dist/js/index.js
+++ dist/js/index.js
@@ -30,36 +30,33 @@ function salvarCadastro() {
         c_data_nascimento.setDate(c_data_nascimento.getDate() + 1); // ajustando bug de fuzo-horário
         let c_data_reforco = new Date(c_data_vacina); //copiando a data de vacinação
         c_data_reforco.setDate(c_data_vacina.getDate() + 30); // adicionando 30 dias
+        const pessoa = { cpf: c_cpf, nome_pessoa: c_nome_pessoa, data_nascimento: formataData(c_data_nascimento),
+            nome_vacina: c_nome_vacina, data_vacina: formataData(c_data_vacina), data_reforco: formataData(c_data_reforco) };
         //adicionando pessoas no array
-        array_pessoas.push({ cpf: c_cpf, nome_pessoa: c_nome_pessoa, data_nascimento: formataData(c_data_nascimento),
-            nome_vacina: c_nome_vacina, data_vacina: formataData(c_data_vacina), data_reforco: formataData(c_data_reforco) });
+        array_pessoas.push(pessoa);
+        mostrarNaPagina(pessoa);
     }
     console.log(array_pessoas);
 }
-function mostrarNaPagina() {
+function mostrarNaPagina(pessoa) {
     const pessoa_cadastrada = document.createElement("div");
-    pessoa_cadastrada.id = ("pessoa_cadastrada");
     document.getElementById('dados_cadastrados').appendChild(pessoa_cadastrada);
-    for (let index = 0; index < array_pessoas.length; index++) {
-        const pessoa = array_pessoas[index];
-        const n_cpf = document.createTextNode("CPF: " + pessoa.cpf);
-        const n_nome_pessoa = document.createTextNode("NOME: " + pessoa.nome_pessoa);
-        const n_data_nascimento = document.createTextNode("DATA DE NASCIMENTO: " + pessoa.data_nascimento);
-        const n_nome_vacina = document.createTextNode("VACINA: " + pessoa.nome_vacina);
-        const n_data_vacina = document.createTextNode("DATA DE VACINAÇÃO: " + pessoa.data_vacina);
-        const n_data_reforco = document.createTextNode("DATA DE REFORCO: " + pessoa.data_reforco);
-        const nodes = [n_cpf, n_nome_pessoa, n_data_nascimento, n_nome_vacina,
-            n_data_vacina, n_data_reforco];
-        for (let n = 0; n < nodes.length; n++) {
-            const paragrafo = document.createElement("p");
-            paragrafo.appendChild(nodes[n]);
-            document.getElementById("pessoa_cadastrada").appendChild(paragrafo);
-        }
+    const n_cpf = document.createTextNode("CPF: " + pessoa.cpf);
+    const n_nome_pessoa = document.createTextNode("NOME: " + pessoa.nome_pessoa);
+    const n_data_nascimento = document.createTextNode("DATA DE NASCIMENTO: " + pessoa.data_nascimento);
+    const n_nome_vacina = document.createTextNode("VACINA: " + pessoa.nome_vacina);
+    const n_data_vacina = document.createTextNode("DATA DE VACINAÇÃO: " + pessoa.data_vacina);
+    const n_data_reforco = document.createTextNode("DATA DE REFORCO: " + pessoa.data_reforco);
+    const nodes = [n_cpf, n_nome_pessoa, n_data_nascimento, n_nome_vacina, n_data_vacina, n_data_reforco];
+    for (let n = 0; n < nodes.length; n++) {
+        const paragrafo = document.createElement("p");
+        paragrafo.appendChild(nodes[n]);
+        pessoa_cadastrada.appendChild(paragrafo);
     }
+    pessoa_cadastrada.appendChild(document.createElement("hr"));
 }
 const formulario = document.getElementById("cadastro-form");
 formulario.addEventListener("submit", function (evento) {
     evento.preventDefault();
     salvarCadastro();
-    mostrarNaPagina();
 });
--- src/index.ts
+++ src/index.ts
@@ -47,52 +47,45 @@ function salvarCadastro() : void{
         let c_data_reforco : Date  = new Date(c_data_vacina) //copiando a data de vacinação
         c_data_reforco.setDate(c_data_vacina.getDate() + 30) // adicionando 30 dias
         
-
+        const pessoa : I_pessoa = {cpf: c_cpf, nome_pessoa: c_nome_pessoa, data_nascimento: formataData(c_data_nascimento), 
+            nome_vacina: c_nome_vacina, data_vacina: formataData(c_data_vacina), data_reforco: formataData(c_data_reforco)}
+        
         //adicionando pessoas no array
-        array_pessoas.push({cpf: c_cpf, nome_pessoa: c_nome_pessoa, data_nascimento: formataData(c_data_nascimento), 
-        nome_vacina: c_nome_vacina, data_vacina: formataData(c_data_vacina), data_reforco: formataData(c_data_reforco)}) 
-
+        array_pessoas.push(pessoa) 
+        
+        mostrarNaPagina(pessoa)
     }
     console.log(array_pessoas)
+
 }
 
 
-function mostrarNaPagina() : void{
+function mostrarNaPagina(pessoa : I_pessoa) : void{
     const pessoa_cadastrada : HTMLDivElement  = document.createElement("div")
-    
-    pessoa_cadastrada.id = ("pessoa_cadastrada")
 
     document.getElementById('dados_cadastrados')!.appendChild(pessoa_cadastrada)
-
-    for (let index = 0; index < array_pessoas.length; index++) {
-        const pessoa : I_pessoa = array_pessoas[index];
-
-        const n_cpf             = document.createTextNode("CPF: " + pessoa.cpf)
-        const n_nome_pessoa     = document.createTextNode("NOME: " + pessoa.nome_pessoa)
-        const n_data_nascimento = document.createTextNode("DATA DE NASCIMENTO: " + pessoa.data_nascimento)
-        const n_nome_vacina     = document.createTextNode("VACINA: " + pessoa.nome_vacina)
-        const n_data_vacina     = document.createTextNode("DATA DE VACINAÇÃO: " + pessoa.data_vacina)
-        const n_data_reforco    = document.createTextNode("DATA DE REFORCO: " + pessoa.data_reforco)
+    const n_cpf             = document.createTextNode("CPF: " + pessoa.cpf)
+    const n_nome_pessoa     = document.createTextNode("NOME: " + pessoa.nome_pessoa)
+    const n_data_nascimento = document.createTextNode("DATA DE NASCIMENTO: " + pessoa.data_nascimento)
+    const n_nome_vacina     = document.createTextNode("VACINA: " + pessoa.nome_vacina)        
+    const n_data_vacina     = document.createTextNode("DATA DE VACINAÇÃO: " + pessoa.data_vacina)
+    const n_data_reforco    = document.createTextNode("DATA DE REFORCO: " + pessoa.data_reforco)
         
+    const nodes : Text[] = [n_cpf, n_nome_pessoa, n_data_nascimento, n_nome_vacina, n_data_vacina, n_data_reforco]
 
-        const nodes : Text[] = [n_cpf, n_nome_pessoa, n_data_nascimento, n_nome_vacina, 
-                                n_data_vacina, n_data_reforco]
 
+    for(let n = 0; n < nodes.length; n++){
+        const paragrafo : HTMLParagraphElement = document.createElement("p")
+        paragrafo.appendChild(nodes[n])
+        pessoa_cadastrada.appendChild(paragrafo) 
             
-        for(let n = 0; n < nodes.length; n++){
-            const paragrafo : HTMLParagraphElement = document.createElement("p")
-            paragrafo.appendChild(nodes[n])
-            document.getElementById("pessoa_cadastrada")!.appendChild(paragrafo) 
-            
-        }
     }
+    pessoa_cadastrada.appendChild(document.createElement("hr"))
     
+
 }
-
-
 const formulario : HTMLElement = document.getElementById("cadastro-form")!;
 formulario.addEventListener("submit", function(evento) : void { //verifica se o cadastro foi enviado
     evento.preventDefault()
     salvarCadastro()
-    mostrarNaPagina()
 })
