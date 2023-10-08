3d39115f0f36622a7c4a7e029089121f92ebfd14
Ariel-Alejandr0 <pessoal.ariel.alejandro@gmail.com>
2023-10-07 23:24:06 -0300

    data de reforço funcionando

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,11 @@ function salvarCadastro() : void{
 
 
     if(verificarData(c_data_vacina) == true){
-        const c_data_reforco = new Date((c_data_vacina.setDate(c_data_vacina.getDate() + 30)).toString())
+        console.log(c_data_vacina)
+        let c_data_reforco : Date  = new Date(c_data_vacina) //copiando a data de vacinação
+        c_data_reforco.setDate(c_data_vacina.getDate() + 30) // adicionando 30 dias
+        console.log(c_data_reforco)
+        
         
         array_pessoas.push({cpf: c_cpf, nome_pessoa: c_nome_pessoa, data_nascimento: c_data_nascimento, 
             nome_vacina: c_nome_vacina, data_vacina: c_data_vacina, data_reforco: c_data_reforco})
